a9818939abc6a3905b1c24393399f657e64cb966
aazhiyath <aazhiyath@gmail.com>
2022-11-12 11:04:36 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const swiper = new swiper('.swiper', {
+    direction: 'vertical',
+    loop: true,
+    
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+   
+  });
+  
